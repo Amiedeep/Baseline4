9fd6eb576e83e1d70a47859b20149d408b66e111
Amandeep Singh <amandees@thoughtworks.com>
2015-09-10 12:01:20 +0530

    Extracting the sales tax calculated into a separate field of class product..now each product knows about the salesTax it have

--- src/com/thoughtworks/baseline4/Product.java
+++ src/com/thoughtworks/baseline4/Product.java
@@ -6,6 +6,7 @@ import static java.lang.Math.round;
 public class Product {
 
     private double price;
+    private double salesTax;
 
     public Product(double price) {
         this.price = price;
@@ -16,11 +17,13 @@ public class Product {
         double taxAmount = (price*10)/100;
         taxAmount = (int)(taxAmount*100);
         if(taxAmount%10 == 5) {
-            return (double)taxAmount/100;
+            salesTax = (double)taxAmount/100;
+            return salesTax;
         }
         else {
             taxAmount = (int)round((double)taxAmount / 10);
-            return (double)taxAmount/10;
+            salesTax = (double)taxAmount/10;
+            return salesTax;
         }
     }
 }
